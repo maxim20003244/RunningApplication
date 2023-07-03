85644b269d39ce2399be0de3de94043bcbdfc07e
Maxim Rotar <maxim20003244@gmail.com>
2023-07-03 22:33:11 +0100

    Crete User and Role table .ManyToMany relationship between table.

---
+++ src/main/java/com/rungroup/web/runninapplication/models/Role.java
@@ -0,0 +1,23 @@
+package com.rungroup.web.runninapplication.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name ="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private List<UserEntity> userEntityList = new ArrayList<>();
+}
